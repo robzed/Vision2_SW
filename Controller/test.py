94c95feb2ad92abe87b6cf9a3d4a09b58353c765
Rob Probin <rob.probin@gmail.com>
2016-04-10 17:50:10 +0000

    Fixes for various issues

--- Controller/test.py
+++ Controller/test.py
@@ -7,15 +7,16 @@ import time
 
 def main():
     port = serial.Serial("/dev/ttyAMA0", baudrate = 57600, timeout = 2)
-    bytes_waiting = port.in_waiting()
+    bytes_waiting = port.inWaiting()
     print(bytes_waiting)
     s = port.read(bytes_waiting)
     print(":".join("{:02x}".format(ord(c)) for c in s))
 
-    report.write(b'\xFE\xFC\xF8\xFE')
-    s = port.read(10)
-    print(len(s)
-    print(":".join("{:02x}".format(ord(c)) for c in s))
+    port.write(b'\xFE\xFC\xF8\xFE')
+    while True:
+        s = port.read(10)
+        print(len(s))
+        print(":".join("{:02x}".format(ord(c)) for c in s))
     
 main()
 

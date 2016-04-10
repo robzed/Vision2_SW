c6eb51228337e0c803af339a5d24f11d3bbc3d67
Rob Probin <rob.probin@gmail.com>
2016-04-10 20:46:46 +0100

    Added Poll and LED 1 & 3 on commands to test

--- Controller/test.py
+++ Controller/test.py
@@ -6,13 +6,30 @@ import serial
 import time
 
 def main():
-    port = serial.Serial("/dev/ttyAMA0", baudrate = 57600, timeout = 2)
+    port = serial.Serial("/dev/ttyAMA0", baudrate = 57600, timeout = 1)
     bytes_waiting = port.inWaiting()
     print(bytes_waiting)
     s = port.read(bytes_waiting)
     print(":".join("{:02x}".format(ord(c)) for c in s))
 
     port.write(b'\xFE\xFC\xF8\xFE')
+    for i in range(3):
+        s = port.read(10)
+        print(len(s))
+        print(":".join("{:02x}".format(ord(c)) for c in s))
+
+    port.write(b'\x80')
+    for i in range(3):
+        s = port.read(10)
+        print(len(s))
+        print(":".join("{:02x}".format(ord(c)) for c in s))
+        
+    port.write(b'\x11\x13')
+    for i in range(3):
+        s = port.read(10)
+        print(len(s))
+        print(":".join("{:02x}".format(ord(c)) for c in s))
+
     while True:
         s = port.read(10)
         print(len(s))

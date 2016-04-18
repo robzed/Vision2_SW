f785a397a990e5ddd4614d927f5bb5187dcca623
Rob Probin <rob.probin@gmail.com>
2016-04-18 21:24:09 +0100

    Enhancements to low level emulator.

--- Controller/low_level_emulator.py
+++ Controller/low_level_emulator.py
@@ -3,10 +3,20 @@ from collections import deque
 
 class serial:
     class Serial:
+        
         def __init__(self, path, baudrate = 57600, timeout = 0.1):
             self.replies = deque()
             self.locked = True
-        
+            
+            self.heading = 0
+            self.row = 0
+            self.column = 0
+
+            # or maybe just use maze?
+            #self.trans = "   ", "[  ", " _]", 
+            #self.nw_walls = [ # "[_]
+            #                 [1,1,1,1,1,1,1,1,1,1,1,1]
+            #                 ]
         def inWaiting(self):
             return 0
         
@@ -18,23 +28,39 @@ class serial:
                 print("Command", hex(ord(cmd)), "had", len(params), "parameters, not", num_params)
                 exit(1)
         def _process_cmd(self, cmd, params):
+            cmdv = ord(cmd)
             if cmd == "\x20":
                 print("***ALL LEDS:", hex(ord(params[0])))
             elif cmd == "\x21":
                 self._paramcheck(cmd, params, 1)
                 print("***ALL LEDS:", hex(512+ord(params[0])))
-                
+            elif cmdv >= 0x10 and cmdv <= 0x19:
+                print("*** LED", cmdv&0x0F, "on")
+            elif cmdv >= 0x00 and cmdv <= 0x09:
+                print("*** LED", cmdv&0x0F, "on")
             elif cmd == "\x80":
                 self._paramcheck(cmd, params, 0)
                 self._wrdata(cmd)
                 
+            elif cmd == "\x98":
+                self._paramcheck(cmd, params, 0)
+                self._wrdata("\x40")     # no walls
+            
             elif cmd == "\xC0":
                 self._paramcheck(cmd, params, 0)
                 print("***STOP MOTORS")
             
+            elif cmd == "\xC4":
+                self._paramcheck(cmd, params, 2)
+                print("***Set speed to", ord(params[0])*256+ord(params[1]))
+                
             elif cmd == "\xD0":
                 self._paramcheck(cmd, params, 0)
                 print("*** TURN OFF IR***")
+
+            elif cmd == "\xD1":
+                self._paramcheck(cmd, params, 0)
+                print("*** TURN ON IR***")
                 
             elif cmd == "\xfe":
                 self._paramcheck(cmd, params, 3)

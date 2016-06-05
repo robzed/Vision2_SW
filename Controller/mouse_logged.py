81e2db1d1d3f5eb5632a453265593323cc7f1a58
Rob Probin <rob.probin@gmail.com>
2016-06-05 20:44:32 +0100

    Python pre-script for rc.local run on mouse.

---
+++ Controller/mouse_logged.py
@@ -0,0 +1,17 @@
+import datetime
+import os
+import sys
+
+# switch to the Controller directory
+os.chdir(os.path.dirname(os.path.abspath(sys.argv[0])))
+
+# get the time and date as a string
+now = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S-%N')
+
+# redirect stdout and stderr to a file
+sys.stdout = open("mouse_%s.log" % now, 'w')
+sys.stderr = sys.stdout
+
+# run the mouse code
+import mouse
+mouse.main()

dd84cc1643ec771dcc68ce39cceb5d5e2763830b
Rob Probin <rob.probin@gmail.com>
2016-09-10 13:32:49 +0100

    Modified graphic layout to be close to desired.

--- Controller/graphic_emulator.py
+++ Controller/graphic_emulator.py
@@ -26,7 +26,9 @@
 from __future__ import print_function
 
 # Basic Tkinter
-from tkinter import *
+#from tkinter import *
+from Tkinter import *       # could have used impot Tkinter as tk
+import tkFont
 
 # Python 3 version
 #from tkinter import ttk
@@ -41,8 +43,42 @@ from tkinter import *
 #from Tkinter import Canvas
 #from Tkinter import Rect
 
+# Tkinter Canvas
 # http://effbot.org/tkinterbook/canvas.htm
 # http://www.akeric.com/blog/?p=1116
+
+# Tkinter Geometry managers
+# http://effbot.org/zone/tkinter-geometry.htm
+# http://www.python-course.eu/tkinter_layout_management.php
+# http://effbot.org/tkinterbook/pack.htm
+
+class LED(Label):
+    LED_font = "not set"
+    
+    def setup_font(self, parent):
+        if self.__class__.LED_font == "not set":
+            label = Label(parent, text="x")
+            font = tkFont.Font(font=label['font'])
+            #print(font.actual())
+
+            newfont = tkFont.Font(family=font['family'], size=font['size']+4)
+            #print(newfont.actual())
+
+            self.__class__.LED_font = newfont
+            
+    def __init__(self, parent, colour=None):
+        self.setup_font(parent)
+        if colour is None:
+            self.colour = "black"
+        else:
+            self.colour = colour
+        Label.__init__(self, parent, text="○", foreground="grey", font=self.LED_font)
+
+    def on(self):
+        self.config(text="●", foreground=self.colour)
+    
+    def off(self):
+        self.config(text="○", foreground="grey")
 
 class GUI_App(object):
         def __init__(self, width=256, height=256):
@@ -67,8 +103,18 @@ class GUI_App(object):
             self.root.title("Vision2")
             #self.root.geometry("%sx%s"%(self.width, self.height))
 
-            self.canvas = Canvas(self.root, width=self.width, height=self.height)
-            self.canvas.pack()
+            self.front_led = LED(self.root, "red")
+            self.front_led.pack()
+
+            ######################################################
+            self.line2_frame = Frame(self.root)
+
+            self.left_led = LED(self.root, "green")
+            self.left_led.pack(side=LEFT)
+
+            self.canvas = Canvas(self.line2_frame, width=self.width, height=self.height)
+            self.canvas.pack(side=LEFT)
+            self.canvas.create_rectangle(3, 3, self.width, self.height, fill="", outline="grey", dash=(4, 4))
 
             self.canvas.create_line(0, 0, 200, 100)
             self.canvas.create_line(0, 100, 200, 0, fill="red", dash=(4, 4))
@@ -79,15 +125,35 @@ class GUI_App(object):
             self.canvas.itemconfig(canvas_id, text="this is the \n text")
             self.canvas.insert(canvas_id, 14, "new ")
 
-            flash_id = self.canvas.create_text(3, 3, anchor="nw", text="o")
+            self.flash_id = self.canvas.create_text(3, 3, anchor="nw", text="o")
             #self.canvas.itemconfig(flash_id, text="#")
             #self.canvas.insert(flash_id, 14, "new ")
             self.state = False
             
+            self.right_led = LED(self.root, "blue")
+            self.right_led.pack(side=RIGHT)
+            
+            self.line2_frame.pack()
+            
+            ######################################################            
             label0 = Label(self.root, text="Main LEDs")
             label0.pack()
-            LEDs = Label(self.root, text=" ○  ○  ○         ●  ●  ●")
-            LEDs.pack()
+            
+            main_led_frame = Frame(self.root)
+
+            #LEDs = Label(self.root, text=" ○  ○  ○         ●  ●  ●")
+            #LEDs.pack()
+            self.leds = []
+            
+            for i in range(6):
+                led = LED(main_led_frame, "red")
+                led.pack(side=LEFT, ipadx=2)
+                if i == 2:
+                    Label(main_led_frame, text="              ").pack(side=LEFT)
+                self.leds.append(led)
+            
+            main_led_frame.pack()
+            ######################################################
             label = Label(self.root, text="Buttons")
             label.pack()
             button_frame = Frame(self.root)
@@ -97,17 +163,24 @@ class GUI_App(object):
             button_b=Button(button_frame, text="short [b]").pack(side=LEFT)
             button_B=Button(button_frame, text="Long [B]").pack(side=LEFT)
             button_frame.pack()
+            
+            ######################################################
             label2 = Label(self.root, text="System Status")
             label2.pack(side=TOP)
             label3 = Label(self.root, text="Battery = 0.0v")
             label3.pack(side=TOP)
             
+            
+        def run(self):
+            
             def task():
                 self.state = not self.state
                 if self.state:
-                    self.canvas.itemconfig(flash_id, text="#")
+                    self.canvas.itemconfig(self.flash_id, text="#")
+                    self.front_led.on()
                 else:
-                    self.canvas.itemconfig(flash_id, text="o")
+                    self.canvas.itemconfig(self.flash_id, text="o")
+                    self.front_led.off()
                 self.root.after(500, task)  # reschedule event in 2 seconds
             
             self.root.after(500, task)
@@ -116,6 +189,6 @@ class GUI_App(object):
             #mainloop()
 
 if __name__ == "__main__":
-    GUI_App()
-    
+    gui = GUI_App()
+    gui.run()
     
